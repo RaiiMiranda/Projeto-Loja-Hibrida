885aab7d677e49e7ec1c7fbe885a6e67acdb4a05
Raissa Miranda <raissa.miranda1801@gmail.com>
2026-03-10 09:23:06 -0300

    Update index.js

--- ProjetoIntegrador/backend/index.js
+++ ProjetoIntegrador/backend/index.js
@@ -1,19 +1,18 @@
-// Inclui o módulo HTTP (protolo para tranferência de dados), utilizando modelo cliente-servidor
-// Baseado em requisições (GET, POST) e respostas (200, 404, ...)
-const { createServer } = require('node:http');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// Servidor escuta nesse host e porta especificados 
-const hostname = 'localhost';
 const port = 3000;
 
-// Cria o servidor e retornando-o
-const server = createServer((req, res) => {
-  res.statusCode = 200;                        // resposta bem sucedida
-  res.setHeader('Content-Type', 'text/plain'); // define o cabeçalho
-  res.end('Hello World');                      // finaliza a resposta com argumento para end()
+// Configura o Express para servir os arquivos dentro da pasta 'frontend'
+// Permitindo que o HTML encontre o CSS e imagens automaticamente
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+// Rota para a página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
-// Informa que o servidor está rodando
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`); // http://localhost:3000/
+app.listen(port, () => {
+    console.log(`Loja de Instrumentos rodando em http://localhost:${port}`);
 });
